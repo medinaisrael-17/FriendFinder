0dcec84f97df070824498d5c2cffeec3412ce785
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-04-08 17:43:12 -0500

    start

--- server.js
+++ server.js
@@ -8,5 +8,9 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "home.html"));
+});
 
-})
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "survey.html"))
+});
